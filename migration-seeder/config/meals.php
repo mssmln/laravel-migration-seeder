4209c49485fed152177b658e73f2c8fb76815e97
Massimiliano <m.croci.ly@gmail.com>
2021-03-22 17:23:45 +0100

    used even fakers to load the db with fake data

---
+++ migration-seeder/config/meals.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    [
+        'name' => 'gnocchi fritti',
+        'temperature' => 'caldi',
+        'calorie_count' => 120,
+        //'cooked_on' => '02-03-2021'
+    ],
+    [
+        'name' => 'carbonara',
+        'temperature' => 'caldi',
+        'calorie_count' => 130,
+        //'cooked_on' => '03-03-2021'
+    ],
+    [
+        'name' => 'pasta al forno',
+        'temperature' => 'caldi',
+        'calorie_count' => 200,
+        //'cooked_on' => '04-03-2021'
+    ]
+];
--- migration-seeder/database/seeds/MealSeeder.php
+++ migration-seeder/database/seeds/MealSeeder.php
@@ -5,6 +5,7 @@ Dopo creiamo un seeder che popoli la nostra tabella, attraverso l'utilizzo di da
 
 use Illuminate\Database\Seeder;
 use App\Meal; // model
+use Faker\Generator as Faker; // model del faker
 use Carbon\Carbon;
 
 class MealSeeder extends Seeder
@@ -14,28 +15,10 @@ class MealSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+
+    /*public function run() // metodo con seeders
     {
-        $meals = [
-            [
-                'name' => 'gnocchi fritti',
-                'temperature' => 'caldi',
-                'calorie_count' => 120,
-                //'cooked_on' => '02-03-2021'
-            ],
-            [
-                'name' => 'carbonara',
-                'temperature' => 'caldi',
-                'calorie_count' => 130,
-                //'cooked_on' => '03-03-2021'
-            ],
-            [
-                'name' => 'pasta al forno',
-                'temperature' => 'caldi',
-                'calorie_count' => 200,
-                //'cooked_on' => '04-03-2021'
-            ]
-        ];
+        $meals = config('meals');
 
         foreach($meals as $meal){
             $newMeal = new Meal(); // il model serve per poter creare istanze
@@ -45,6 +28,20 @@ class MealSeeder extends Seeder
             //$newMeal->cooked_on = Carbon::createFromFormat('d/m/Y', $meal['cooked_on']);
 
             $newMeal->save();
+        }
+    }*/
+
+    public function run(Faker $faker) // metodo con fakers
+    {
+        for($i= 0; $i < 10; $i++){
+            $newMeal = new Meal(); 
+            $newMeal->name = $faker->name(20);
+            $newMeal->temperature = $faker->word();
+            $newMeal->calorie_count = $faker->numberBetween(-100,100);
+            //$newMeal->cooked_on = Carbon::createFromFormat('d/m/Y', $meal['cooked_on']);
+
+            $newMeal->save();
+
         }
     }
 }
